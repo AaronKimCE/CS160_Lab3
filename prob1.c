0f544f17ac2e303d04c45ec7b73da0a680b2eb9b
Aaron Kim <akim106@wch129-10.cs.ucr.edu>
2019-11-19 18:17:11 -0800

    Finished prob1

--- prob1.c
+++ prob1.c
@@ -5,11 +5,15 @@
 #include <stdlib.h>
 
 void *thread (void *vargp) {
-  printf("Hello, world\n"); 
+  printf("Hello, World\n"); 
   pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
+  if (argc <= 1) {
+    printf("Must have more than 1 thread.\n");
+    return 0;
+  }
   int n = atoi(argv[1]);
   int i = 0;
   pthread_t tid[n];
@@ -17,4 +21,5 @@ int main (int argc, char *argv[]) {
     pthread_create(&tid[i], NULL, thread, NULL);
     pthread_join(tid[i], NULL);
   }
+  return 0;
 }
